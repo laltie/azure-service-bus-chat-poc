3f73f5c5dd9699dd68712395fe0e257375a7f061
Leonardo Altieri <leonardo.altieri-ext@prada.com>
2021-11-18 11:28:30 +0100

    First commit

---
+++ Models/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusChatPOC.Models
+{
+    public class Message
+    {
+        public Message(string username, string body, bool mine)
+        {
+            Username = username;
+            Body = body;
+            Mine = mine;
+        }
+
+        public string Username { get; set; }
+        public string Body { get; set; }
+        public bool Mine { get; set; }
+
+        public bool IsNotice => Body.StartsWith("[Notice]");
+
+        public string CSS => Mine ? "sent" : "received";
+    }
+}
